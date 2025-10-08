382e57b29e34cb9b61f2e3589bc575db12c60a69
Ash <amorrow1@wgu.edu>
2025-10-08 02:04:03 -0500

    Update proj.sql

--- proj.sql
+++ proj.sql
@@ -1,4 +1,5 @@
 -- PART B: custom transformation with a user-defined function to transform the date field in the detailed table to a written format for better readability.  
+
 CREATE OR REPLACE FUNCTION fn_format_rental_date(date_input DATE) 
 RETURNS TEXT AS $$ 
 BEGIN 
@@ -7,7 +8,9 @@ END;
 $$ LANGUAGE plpgsql;  
 
 -- PART C: creates detailed and summary tables.  
+
 -- Detailed report table 
+
 CREATE TABLE genre_rental_detailed ( 
  rental_id INTEGER, 
  rental_date TEXT, 
@@ -16,12 +19,14 @@ CREATE TABLE genre_rental_detailed (
 );  
 
 -- Summary report table 
+
 CREATE TABLE genre_rental_summary ( 
  genre TEXT PRIMARY KEY, 
  total_rentals INTEGER 
 );  
 
 -- PART D: extracts raw data needed for the detailed data table. 
+
 INSERT INTO genre_rental_detailed (rental_id, rental_date, title, genre) 
 SELECT 
  r.rental_id, 
@@ -33,7 +38,8 @@ JOIN film f ON i.film_id = f.film_id
 JOIN film_category fc ON f.film_id = fc.film_id 
 JOIN category c ON fc.category_id = c.category_id;  
 
--- PART E: creates a trigger on the detailed table that continually updates the summary table as data is added to the detailed table.  
+-- PART E: creates a trigger on the detailed table that continually updates the summary table as data is added to the detailed table.
+
 CREATE OR REPLACE FUNCTION trg_update_summary() 
 RETURNS TRIGGER AS $$ 
 BEGIN 
@@ -45,7 +51,9 @@ RETURN NEW;
 END; 
 $$ LANGUAGE plpgsql;  
 
--- Trigger CREATE TRIGGER trg_after_insert_detailed AFTER INSERT ON genre_rental_detailed 
+-- Trigger CREATE TRIGGER 
+
+trg_after_insert_detailed AFTER INSERT ON genre_rental_detailed 
 FOR EACH ROW 
 EXECUTE FUNCTION trg_update_summary();  
 
